bccfbe203fa512d2ddc7e0ef4aa98f89924a0426
Pheonvova <pheon1310@gmail.com>
2020-05-14 00:49:02 +0300

    Initial Commit

---
+++ Twoviewsapp(learning)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twoviewsapp(learning)
+//
+//  Created by Pheon on 14.05.2020.
+//  Copyright © 2020 Pheon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
